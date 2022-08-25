775c64bba91aac61667a72acdba8cc667b3f4c2c
Muhammad Hanafi Firdaus <hanafi.firdaus17@gmail.com>
2022-08-25 17:02:24 +0700

    on progress build codebase

--- main.go
+++ main.go
@@ -14,15 +14,19 @@ import (
 	"syscall"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/soheilhy/cmux"
+	"github.com/widyan/go-codebase/config"
 	"github.com/widyan/go-codebase/helper"
-	initial "github.com/widyan/go-codebase/modules/domain-grpc"
+	domaingrpc "github.com/widyan/go-codebase/modules/domain-grpc"
 	pb "github.com/widyan/go-codebase/modules/domain-grpc/proto/v1"
 	"github.com/widyan/go-codebase/notification"
 	"github.com/widyan/go-codebase/responses"
 	"go.elastic.co/apm/module/apmgrpc"
+	"go.elastic.co/apm/module/apmhttp"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 
@@ -68,7 +72,7 @@ func main() {
 	vldt := validate.CreateValidator(validator)
 	// *************************************************************************************************
 
-	// var cfg config.Config
+	var cfg config.Config
 
 	/*
 		// ************************************ Config for implement DB ************************************
@@ -107,8 +111,8 @@ func main() {
 		return nil
 	})
 
-	g.Go(func() error { return initial.Init(logger, vldt, nil, response) })
-	g.Go(func() error { return NewHTTPServer() })
+	g.Go(func() error { return NewGRPCServer(logger, cfg, vldt, response) })
+	g.Go(func() error { return NewHTTPServer(logger) })
 
 	if err := g.Wait(); !IgnoreErr(err) {
 		panic(err)
@@ -116,11 +120,30 @@ func main() {
 
 }
 
-func NewGRPCServer() error {
+func NewGRPCServer(logger *logrus.Logger, cfg config.Config, validator validate.ValidatorInterface, cfgRseponses responses.GinResponses) (err error) {
+	// register grpc service server
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(apmgrpc.NewUnaryServerInterceptor(apmgrpc.WithRecovery())),
+		grpc.StreamInterceptor(apmgrpc.NewStreamServerInterceptor()),
+	)
+
+	if err = domaingrpc.Init(logger, grpcServer, cfg, validator, cfgRseponses); err != nil {
+		return
+	}
+
+	lis, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		return
+	}
+
+	reflection.Register(grpcServer)
+
+	logger.Println("[SERVER] GRPC server is ready")
+	grpcServer.Serve(lis)
 	return nil
 }
 
-func NewHTTPServer() error {
+func NewHTTPServer(logger *logrus.Logger) error {
 	/*
 		tracer.Start(tracer.WithDebugMode(true))
 		defer tracer.Stop()
@@ -138,7 +161,7 @@ func NewHTTPServer() error {
 		grpc.WithUnaryInterceptor(apmgrpc.NewUnaryClientInterceptor()),
 	)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		logger.Fatalf("fail to dial: %v", err)
 		panic(err)
 	}
 	defer conn.Close()
@@ -169,8 +192,8 @@ func NewHTTPServer() error {
 	// create http server mux
 	mux := http.NewServeMux()
 	mux.Handle("/", rmux)
-	// mux.Handle("/metrics", promhttp.Handler())
-	// apmhttp.Wrap(mux)
+	mux.Handle("/metrics", promhttp.Handler())
+	apmhttp.Wrap(mux)
 
 	/*
 		// run swagger server
@@ -180,12 +203,12 @@ func NewHTTPServer() error {
 	*/
 
 	// running delivery http server
-	log.Println("[SERVER] REST HTTP server is ready")
+	logger.Println("[SERVER] REST HTTP server is ready")
 
 	http.ListenAndServe(":7000", mux)
 
 	if err != nil {
-		log.Println(err.Error())
+		logger.Println(err.Error())
 		return err
 	}
 
--- modules/domain-grpc/init.go
+++ modules/domain-grpc/init.go
@@ -1,14 +1,9 @@
 package domain
 
 import (
-	"database/sql"
-	"log"
-	"net"
-
+	"github.com/widyan/go-codebase/config"
 	"github.com/widyan/go-codebase/responses"
-	"go.elastic.co/apm/module/apmgrpc"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 
 	"github.com/widyan/go-codebase/modules/domain-grpc/handler"
 	pb "github.com/widyan/go-codebase/modules/domain-grpc/proto/v1"
@@ -19,29 +14,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Init(logger *logrus.Logger, validator validate.ValidatorInterface, pq *sql.DB, cfgRseponses responses.GinResponses) (err error) {
-
-	repo := repository.CreateRepository(pq, pq, logger) // Create transaction from db
+func Init(logger *logrus.Logger, grpcServer *grpc.Server, cfg config.Config, validator validate.ValidatorInterface, cfgRseponses responses.GinResponses) (err error) {
+	repo := repository.CreateRepository(nil, nil, logger) // Create transaction from db
 	userUsecase := usecase.CreateUsecase(repo, logger)
-
-	// register grpc service server
-	grpcServer := grpc.NewServer(
-		grpc.UnaryInterceptor(apmgrpc.NewUnaryServerInterceptor(apmgrpc.WithRecovery())),
-		grpc.StreamInterceptor(apmgrpc.NewStreamServerInterceptor()),
-	)
 
 	hndlr := handler.CreateHandler(userUsecase, logger, cfgRseponses, validator)
 	pb.RegisterTestServer(grpcServer, hndlr)
-
-	lis, err := net.Listen("tcp", ":5000")
-	if err != nil {
-		return
-	}
-
-	reflection.Register(grpcServer)
-
-	log.Println("[SERVER] GRPC server is ready")
-	grpcServer.Serve(lis)
 
 	return
 }
